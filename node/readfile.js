6de39823069bbe3b85029df51d7cdd227c7dd845
wyc <844544239@qq.com>
2019-03-16 17:03:00 +0800

    admin

--- node/readfile.js
+++ node/readfile.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-fs.readFile('./file/test.txt','utf-8',function(err,data){
+fs.readFile('./file/test.txt','utf-8',function(err,data){//异步
     if(err){
         console.log('文件读取失败');
         
@@ -7,4 +7,7 @@ fs.readFile('./file/test.txt','utf-8',function(err,data){
         console.log(data);
     }
 })
-console.log('daf');
+let data = fs.readFileSync('./file/test.txt','utf-8');//同步
+console.log(data);
+
+console.log('读取文件：');
