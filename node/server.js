ac8f085d5f3bcfaef0b5ca195837d6fcaf1bd3cc
wyc <844544239@qq.com>
2019-03-17 17:07:38 +0800

    admin

--- node/server.js
+++ node/server.js
@@ -1,9 +1,18 @@
 var http = require('http')
 var url = require('url')
 http.createServer(function(req,res){
-    
-    res.write('<h1>node1.js</h1>');
-    res.write('<h1>node2.js</h1>');
-    res.end('<h1>hello world</h1>');
+    console.log(req.url);
+    console.log(url.parse(req.url).pathname);
+    let path = url.parse(req.url).pathname;
+    if(path=='/'){
+        res.end('home');
+    }else if(path == '/about'){
+        res.end('about');
+    }else{
+        res.end('404');
+    }
+    // res.write('<h1>node1.js</h1>');
+    // res.write('<h1>node2.js</h1>');
+    //res.end('<h1>hello world</h1>');
 }).listen('3000')
 console.log('服务运行在3000端口')
